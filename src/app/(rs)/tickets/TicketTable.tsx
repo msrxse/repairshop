1be2449062fb75c2126d9ae3a58fe90453424180
msr <msrxse@gmail.com>
2025-07-14 13:46:08 +0100

    Fix pagination bar css on small screens

--- src/app/(rs)/tickets/TicketTable.tsx
+++ src/app/(rs)/tickets/TicketTable.tsx
@@ -51,6 +51,9 @@ export default function TicketTable({ data }: Props) {
   const pageIndex = useMemo(() => {
     const page = searchParams.get("page");
     return page ? parseInt(page) - 1 : 0;
+
+    // only interested in this particular searchParam
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchParams.get("page")]);
 
   const columnHeadersArray: Array<keyof RowType> = [
@@ -193,8 +196,8 @@ export default function TicketTable({ data }: Props) {
           </TableBody>
         </Table>
       </div>
-      <div className="flex justify-between items-center">
-        <div className="flex basics-1/3 items-center">
+      <div className="flex justify-between items-center gap-1 flex-wrap">
+        <div>
           <p className="whitespace-nowrap font-bold">
             {`Page ${
               table.getState().pagination.pageIndex + 1
@@ -207,42 +210,49 @@ export default function TicketTable({ data }: Props) {
             }]`}
           </p>
         </div>
-        <div className="space-x-1">
-          <Button variant="outline" onClick={() => router.refresh()}>
-            Refresh Data
-          </Button>
-          <Button variant="outline" onClick={() => table.resetSorting()}>
-            Reset Sorting
-          </Button>
-          <Button variant="outline" onClick={() => table.resetColumnFilters()}>
-            Reset Filters
-          </Button>
-          <Button
-            variant="outline"
-            onClick={() => {
-              const newIndex = table.getState().pagination.pageIndex - 1;
-              table.setPageIndex(newIndex);
-              const params = new URLSearchParams(searchParams.toString());
-              params.set("page", (newIndex + 1).toString());
-              router.replace(`?${params.toString()}`, { scroll: false });
-            }}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            onClick={() => {
-              const newIndex = table.getState().pagination.pageIndex + 1;
-              table.setPageIndex(newIndex);
-              const params = new URLSearchParams(searchParams.toString());
-              params.set("page", (newIndex + 1).toString());
-              router.replace(`?${params.toString()}`, { scroll: false });
-            }}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
+        <div className="flex flex-row gap-1">
+          <div className="flex flex-row gap-1">
+            <Button variant="outline" onClick={() => router.refresh()}>
+              Refresh Data
+            </Button>
+            <Button variant="outline" onClick={() => table.resetSorting()}>
+              Reset Sorting
+            </Button>
+            <Button
+              variant="outline"
+              onClick={() => table.resetColumnFilters()}
+            >
+              Reset Filters
+            </Button>
+          </div>
+          <div className="flex flex-row gap-1">
+            <Button
+              variant="outline"
+              onClick={() => {
+                const newIndex = table.getState().pagination.pageIndex - 1;
+                table.setPageIndex(newIndex);
+                const params = new URLSearchParams(searchParams.toString());
+                params.set("page", (newIndex + 1).toString());
+                router.replace(`?${params.toString()}`, { scroll: false });
+              }}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              onClick={() => {
+                const newIndex = table.getState().pagination.pageIndex + 1;
+                table.setPageIndex(newIndex);
+                const params = new URLSearchParams(searchParams.toString());
+                params.set("page", (newIndex + 1).toString());
+                router.replace(`?${params.toString()}`, { scroll: false });
+              }}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
+          </div>
         </div>
       </div>
     </div>
